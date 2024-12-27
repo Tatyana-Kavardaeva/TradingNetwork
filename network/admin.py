47083b6ef8f30e8e26c290c208d850ff1db395f0
Tatyana Kavardaeva <tatyana.kavardaeva@yandex.ru>
2024-12-27 15:57:41 +0300

    updated network\admin.py added @admin.action

--- network/admin.py
+++ network/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
-
 from network.models import Company, Product
 
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'supplier', 'country', 'city')
-    list_display_links = ('supplier',)
-    list_filter = ('name', 'city')
+    list_display = ('id', 'name', 'supplier', 'country', 'city', 'company_level')
+    list_display_links = ('id', 'supplier',)
+    list_filter = ('city',)
+
+    actions = ('reset_debt',)
+
+    @admin.action(description='Очистить значение "Задолженность перед поставщиком"')
+    def reset_debt(self, request, queryset):
+        queryset.update(debt=0)
+        self.message_user(request, 'Значение "Задолженность перед поставщиком" успешно приведено к нулю.')
 
 
 @admin.register(Product)
